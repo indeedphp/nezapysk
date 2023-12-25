7434615af058a637c30f93e82a6bad417a26f39a
indeedphp <ntsiti@gmail.com>
2023-12-25 21:35:44 +0600

    firs55

--- sw.js
+++ sw.js
@@ -1,15 +1,48 @@
-const staticCacheName = 's-app-v3'
 
-const assetUrls = [
-  'index.html'
-]
+const staticCache = 'site-static-1';
+const assets = [
+'index.html'
+];
 
-self.addEventListener('install', event => {
- event.waitUntil(
-  caches.open(staticCacheName).then(cache => cache.addAll(assetUrls) )
- )
-})
+self.addEventListener('install', evt => {
+    console.log('Service worker has been installed');
 
-self.addEventListener('activate', event => {
-console.log('Service worker activate')
-})
+    self.skipWaiting();
+
+
+    evt.waitUntil(
+        caches.open(staticCache).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== staticCache) {
+                        return caches.delete(cache); //Deleting the old cache (cache v1)
+                    }
+                })
+            );
+        })
+            .then(function () {
+                console.info("Old caches are cleared!");
+                return self.clients.claim();
+            })
+    );
+});
+
+
+
+self.addEventListener('fetch', evt => {
+    console.log('SW is fetching data');
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request)  // if item is in cache use it, if isn't go to the server and fetch it
+        })
+    )
+});
